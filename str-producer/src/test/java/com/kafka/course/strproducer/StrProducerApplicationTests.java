153451e46f991a24b12a5b9374d20a5ad3d697f2
Raphael Nacarato <nacarato.raphael@gmail.com>
2023-08-22 09:50:05 -0300

    add callback function to sendMessage function

---
+++ str-producer/src/test/java/com/kafka/course/strproducer/StrProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.course.strproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
