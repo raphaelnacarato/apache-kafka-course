6ba22b6c185fcc5e149b18e70bc215b189ac9123
Raphael Nacarato <nacarato.raphael@gmail.com>
2023-08-22 15:56:33 -0300

    add json consumer

---
+++ json-consumer/src/test/java/com/kafka/course/jsonconsumer/JsonConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafka.course.jsonconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
